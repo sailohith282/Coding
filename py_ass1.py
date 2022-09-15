5b940b0761f46e8029e2715d7b8c3bd7fca297ca
Sai Lohith T <89346893+sailohith282@users.noreply.github.com>
2022-09-15 23:15:32 +0530

    Update py_ass1.py

--- py_ass1.py
+++ py_ass1.py
@@ -1,3 +1,9 @@
+#Write a Python program which takes two digits m (row) and n (column) as input and generates a two-dimensional array.
+#The element value in the i-th row and j-th column of the array should be i*j.
+#Note :
+#i = 0,1.., m-1
+#j = 0,1, n-1.
+#the code is 
 row_num = int(input("Enter number of rows needed: "))
 col_num = int(input("Enter number of columns needed: "))
 multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
